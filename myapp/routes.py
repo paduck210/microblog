85b0abb56fc1965d73a705480f84694677be2100
Jung Yu <hjyu1408@gmail.com>
2020-01-05 18:42:30 -0500

    "Templates, make base and index page with varialbe"

---
+++ myapp/routes.py
@@ -0,0 +1,19 @@
+from myapp import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'username': 'paduck' }
+    title = "Home"
+    posts = [
+        {
+            'author': { 'username' : 'John'},
+            'body' : "Harry potter is fun to read, but he has't read it"
+        },
+        {
+            'author': { 'username': 'Mary' },
+            'body' : 'My chin condition is not good'
+        }
+    ]
+    return render_template('index.html', title = title, user=user, posts=posts)
