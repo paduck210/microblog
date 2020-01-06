293138ac37fa5988da9079259070f110ef55ec9d
Jung Yu <hjyu1408@gmail.com>
2020-01-06 11:59:36 -0500

    "Receiving data, set Flash message"

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template, flash, redirect
 from myapp import app
-from flask import render_template
 from myapp.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,7 +20,11 @@ def index():
     ]
     return render_template('index.html', title = title, user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
