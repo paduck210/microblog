2cbbcd01a2b2ef6590a4ba4a6cf9ee00d921225f
Jung Yu <hjyu1408@gmail.com>
2020-01-06 17:07:40 -0500

    "Make two models and create setting can work with shell"

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from myapp import app
+from myapp import app, db
+from myapp.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
