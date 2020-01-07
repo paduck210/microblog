4ffdaa41a7b7a90a2d042075990828dab5633013
Jung Yu <hjyu1408@gmail.com>
2020-01-07 01:21:12 -0500

    "User setup - log in,, sign up"

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-from myapp import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from myapp import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,6 +13,17 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password) # return True/False
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -20,3 +34,14 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.body)
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, flash, redirect, url_for
-from myapp import app
-from myapp.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import login_user, current_user, logout_user, login_required
+from myapp import app, db
+from myapp.forms import LoginForm, RegistrationForm
+from myapp.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required # only available with log_in user
 def index():
-    user = { 'username': 'paduck' }
-    title = "Home"
     posts = [
         {
             'author': { 'username' : 'John'},
@@ -18,14 +20,50 @@ def index():
             'body' : 'My chin condition is not good'
         }
     ]
-    return render_template('index.html', title = title, user=user, posts=posts)
+    return render_template('index.html', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        # return redirect('/index')
+    if current_user.is_authenticated: # already confirmed as a user
         return redirect(url_for('index'))
+
+    form = LoginForm() # Get Login Form
+
+    if form.validate_on_submit(): # On a Filed, when user click submit button
+        user = User.query.filter_by(username=form.username.data).first() # get a user instance match with username
+
+        if user is None or not user.check_password(form.password.data):  # is username is incorrect or password is wrong
+            flash('Invalid username or password') # show this message on the page
+            return redirect(url_for('login')) # and stay on login page
+
+        login_user(user, remember=form.remember_me.data) # after user success to log in
+        next_page = request.args.get('next') # user redirect to the page who is supposed to visit
+        if not next_page or url_parse(next_page).netloc != '': # get parameter of URL
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:  # already confirmed as a user
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username= form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congrat! you are now a new user of blog')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
