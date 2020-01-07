77bec0edb6f60141fd3a403f36fed515f45cb07d
Jung Yu <hjyu1408@gmail.com>
2020-01-07 01:21:42 -0500

    "IMPORTANT- Change Migration directory"

--- microblog.py
+++ microblog.py
@@ -1,5 +1,19 @@
+import os
+from flask_script import Manager # pip install Flask-Script
+from flask_migrate import Migrate, MigrateCommand
 from myapp import app, db
 from myapp.models import User, Post
+
+
+MIGRATION_DIR = os.path.join('resources')
+
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
 
 @app.shell_context_processor
 def make_shell_context():
