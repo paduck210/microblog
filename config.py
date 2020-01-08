fa5f2a817993edbd2b167405a802a0784e279d21
Jung Yu <hjyu1408@gmail.com>
2020-01-07 20:53:33 -0500

    "Create User login logout"

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
+# TODO: * TASK 6: Implement an ORM database access layer so we don’t have SQL in the controller code.
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '111111'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db') # after migration
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from myapp.models import User
 
 # Model
@@ -29,4 +29,9 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address!')
 
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)]) # Empty field is possible
+    submit = SubmitField('Submit')
 
