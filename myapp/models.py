6b3180f2cdaeff0ac60896391ed8c33f5cc1d662
Jung Yu <hjyu1408@gmail.com>
2020-01-07 20:52:52 -0500

    "Create User profile page"

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from myapp import db, login
 from flask_login import UserMixin
@@ -10,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic') # post.author ==> user instance
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -20,7 +23,13 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password) # return True/False
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
+
+# for login logout setup
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,9 +1,19 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, current_user, logout_user, login_required
 from myapp import app, db
-from myapp.forms import LoginForm, RegistrationForm
+from myapp.forms import LoginForm, RegistrationForm, EditProfileForm
 from myapp.models import User
+
+
+# apply this function to all the routes below
+# update user's last_seen data automatically
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -27,9 +37,7 @@ def index():
 def login():
     if current_user.is_authenticated: # already confirmed as a user
         return redirect(url_for('index'))
-
     form = LoginForm() # Get Login Form
-
     if form.validate_on_submit(): # On a Filed, when user click submit button
         user = User.query.filter_by(username=form.username.data).first() # get a user instance match with username
 
@@ -56,6 +64,8 @@ def register():
     if current_user.is_authenticated:  # already confirmed as a user
         return redirect(url_for('index'))
     form = RegistrationForm()
+
+    # TODO: TASK 4: As a user I can see a confirmation message when I add/delete a todo.
     if form.validate_on_submit():
         user = User(username= form.username.data, email=form.email.data)
         user.set_password(form.password.data)
@@ -66,4 +76,34 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
+@app.route('/user/<username>') # dynamic component
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404() # if user is not founded, raise 404 error page
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+
+    # TODO: TASK 1: As a user I can't add a todo without a description.
+    if form.validate_on_submit(): # POST
+        if form.about_me.data == "":
+            flash("Should fill ABOUT ME")
+            return redirect(url_for('edit_profile'))
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('User information Updated')
+        return redirect(url_for('user', username=current_user.username))
+
+    elif request.method == "GET": # when user open this page, forms already fill with data of the user
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title="Edit Profile", form=form)
